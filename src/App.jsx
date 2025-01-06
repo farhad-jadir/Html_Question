fe1887396977207137049ba33ab8e7b8cbe7bd3d
farhadjony <farhadjony.math@gmail.com>
2025-01-06 21:43:47 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import HtmlInterciew from './Components/Html'
+
+
+function App() {
+
+  return (
+    <>
+     <HtmlInterciew/>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/utils/ClassNames.js
@@ -0,0 +1,8 @@
+
+export const baseParagraphClasses = {
+    question: "text-base sm:text-lg md:text-xl leading-relaxed text-red-300 font-bold ",
+    answer: "text-[16px] sm:text-lg md:text-xl leading-relaxed text-orange-200 ",
+    para: "text-base sm:text-lg md:text-xl leading-relaxed text-white ",
+    heading: "font-semibold sm:font-bold text-xl sm:text-2xl md:text-4xl text-indigo-200 text-center "
+  };
+  
